159f9a2ca9a395ab850d06e2d07c259d93f3ebcf
Jacob Hocker <jacobdouglas06@gmail.com>
2023-03-10 13:09:18 -0600

    First commit

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function head() {
+    return (
+        <>
+            <title>EntSpot</title>
+            <meta name="viewport" content="width=device-width, initial-scale-1" />
+            <meta name="description" content="A place for all your entertainment needs!" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
