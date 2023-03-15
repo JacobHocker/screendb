65243d0f573cfde383ed2ef17581ea47391d1282
Jacob Hocker <jacobdouglas06@gmail.com>
2023-03-15 10:08:42 -0500

    added navbar boiler plate template

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Header from "@/components/Header";
 import Providers from './Providers';
+import Navbar from '@/components/Navbar';
 
 
 export default function RootLayout({ children }) {
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
           <Header/>
 
           {/*NavBar */}
-          
+          <Navbar />
           {/*SearchBox*/}
 
 
