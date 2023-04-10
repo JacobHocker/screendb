80e6d2ca4a7cfbadc4c40e89083e63c896b34263
Jacob Hocker <jacobdouglas06@gmail.com>
2023-04-10 15:12:06 -0500

    added and made responsive the person header

--- src/app/person/[id]/page.jsx
+++ src/app/person/[id]/page.jsx
@@ -1,10 +1,12 @@
 "use client"
 import { useState, useEffect } from 'react';
-
+import Image from 'next/image';
+import { AiOutlineDown,  AiOutlineUp } from 'react-icons/ai';
 
 export default function PersonPage({ params }) {
     const [person, setPerson] = useState({});
-
+    const [extendBio, setExtendBio] = useState(false);
+    
     const personId = params.id;
 
     // Fetching the person information from the database
@@ -14,11 +16,100 @@ export default function PersonPage({ params }) {
         .then((data) => { setPerson(data)})
     }, [personId])
 
+    // Calculate age if person is still alive
+    const getAliveAge = (dateString) => {
+        let today = new Date();
+        let birthDate = new Date(dateString);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        let m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+    // Calculate age if person is no longer alive
+    const getDeadAge = (birth, death) => {
+        let birthDate = new Date(birth);
+        let deathDate = new Date(death);
+        let age = deathDate.getFullYear() - birthDate.getFullYear();
+        let m = deathDate.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && deathDate.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+
+    
+
     console.log(person)
     
     return (
         <div>
-            <h1>Person Page</h1>
+            {
+                person.name && 
+
+                <div className='mt-8 w-full'>
+                    {/* PERSON PAGE HEADER */}
+                    <div className='p-4 md:pt-8 flex flex-col md:grid grid-cols-2 items-center content-center max-w-4xl mx-auto md:space-x-4'>
+                        <Image src={`${process.env.NEXT_PUBLIC_POSTER_PATH}${person.profile_path}`} 
+                        width={500}
+                        height={300}
+                        quality='100'
+                        className="rounded-lg "
+                        style={{
+                            objectFit: "cover",
+                            maxWidth: "100%",
+                            height: "100%",
+                        }}
+                        placeholder="blur"
+                        blurDataURL="/spinner.svg"
+                        alt="Person poster"></Image>
+                        
+                        <div className="p-2">
+                            <h2 className="text-lg  md:text-xl mb-3 font-bold">
+                                {person.name}
+                            </h2>
+                            <p className={`text-lg mb-3 ${extendBio === true ? 'line-clamp-15' : 'line-clamp-8'}`}>
+                                <span className="font-semibold mr-1">Biography:</span>
+                                {person.biography}
+                                
+                            </p>
+                            <div className='w-full text-gray-500 hover:text-amber-600 dark:hover:text-amber-500 p-2 flex justify-center items-center mb-4 cursor-pointer bg-gray-300 bg-opacity-50 dark:bg-gray-600 dark:bg-opacity-50 rounded-lg' onClick={() => {setExtendBio(!extendBio)}}>
+                                {
+                                extendBio === false ?
+                                <h1 className='font-bold'>Learn more...</h1>
+                                :
+                                <AiOutlineUp className='font-bold' />
+                                }
+                            </div>
+                            <p className="mb-3 md:text-lg">
+                                <span className="font-semibold mr-1">Birthday:</span>
+                                {person.birthday} {person.deathday === null ? `(Age: ${getAliveAge(person.birthday)})` : <></>}
+                            </p>
+                            {
+                                person.deathday === null ?
+                                <></> 
+                                :
+                                <p className="mb-3 md:text-lg">
+                                    <span className="font-semibold mr-1">Died:</span>
+                                    {person.deathday} (Age: {getDeadAge(person.birthday, person.deathday)})
+                                </p>
+                            }
+                            <p className="mb-3 md:text-lg">
+                                <span className="font-semibold mr-1">Place of Birth:</span>
+                                {person.place_of_birth}
+                            </p>
+                        </div>
+                    </div>
+                </div> 
+
+                
+
+
+
+            }
+            
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,24 @@ module.exports = {
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
     },
-    extend: {},
+    extend: {
+      lineClamp: {
+        7: '7',
+        8: '8',
+        9: '9',
+        10: '10',
+        11: '11',
+        12: '12',
+        13: '13',
+        14: '14',
+        15: '15',
+        16: '16',
+        17: '17',
+        18: '18',
+        19: '19',
+        20: '20',
+      }
+    },
   },
   variants: {
     lineClamp: ['responsive', 'hover']
